17eaa1ec1d3ac681b7c4c604a618d532bd2078d8
Russel Adrian Lopez <lopez.russeladrian@gmail.com>
2017-10-18 16:30:01 -0400

    feat: setup item model

--- catalog/models.py
+++ catalog/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    brand = models.CharField(max_length=255)
--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Apps
-    'shop',
+    'catalog',
 
     # 3rd party
     'rest_framework',

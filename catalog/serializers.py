da3dd6cb7a65622cd5a29d221947a4eee54dcdde
Russel Adrian Lopez <lopez.russeladrian@gmail.com>
2017-10-18 18:55:04 -0400

    feat: added serializer and input data

---
+++ catalog/serializers.py
@@ -0,0 +1,9 @@
+from .models import Item
+from rest_framework import serializers
+
+
+class ItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'brand')
--- catalog/views.py
+++ catalog/views.py
@@ -5,10 +5,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Item
+from .serializers import ItemSerializer
 # Create your views here.
 
 
 class ItemList(APIView):
     def get(self, request, format=None):
         items = Item.objects.all()
-        return Response(items)
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)

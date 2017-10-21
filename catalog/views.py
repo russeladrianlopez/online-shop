7e7980d5bb7c74da8d7b76302969bb5566d1d925
Russel Adrian Lopez <lopez.russeladrian@gmail.com>
2017-10-20 21:45:36 -0400

    feat: added post method in item list view

--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Item
 from .serializers import ItemSerializer
@@ -14,3 +15,10 @@ class ItemList(APIView):
         items = Item.objects.all()
         serializer = ItemSerializer(items, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
